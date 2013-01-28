cc0103f1c4209370aaf96e3c439f4b04a8209f97
Trippnology <mail@trippnology.com>
2013-01-28 11:45:25 +0000

    Added comments.

--- js/core.js
+++ js/core.js
@@ -1,3 +1,4 @@
+// Programatically add elements
 var addElement = function(name, attrs, innerText, parent) {
 	var element = document.createElement(name);
 	for (a in attrs) {
@@ -12,22 +13,27 @@ var addElement = function(name, attrs, innerText, parent) {
 	return parent.appendChild(element);
 };
 
+// Set a title
 document.head.appendChild(
 	document.createElement('title')
 ).innerText = 'Pinhead';
 
+// Add project stylesheet
 var style = document.head.appendChild(
 	document.createElement('link')
 );
 style.href = 'css/style.css';
 style.rel = 'stylesheet'; // kicks off the request
 
+// Add main library
 var library = document.head.appendChild(
 	document.createElement('script')
 );
+// Do stuff once the library has loaded
 library.addEventListener('load', function () {
 	// now we're in business
 	addElement('h1', null, 'Hello World!');
+	// Use a jQuery method to check it loaded ok
 	$('h1').after('<p>jQuery loaded successfully.</p>');
 });
 library.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'; // kicks off the request
