6703331d29149beaff24eeb8c690fef2d5fdbadd
Trippnology <mail@trippnology.com>
2013-03-21 00:28:50 +0000

    Didn't actually use el.js but did lots of refactoring anyway.

--- js/core.js
+++ js/core.js
@@ -1,11 +1,16 @@
 // Programatically add elements
-var addElement = function(name, attrs, innerText, parent) {
+function addElement(name, attrs, innerText, parent) {
 	var element = document.createElement(name);
 	for (a in attrs) {
 		element.setAttribute(a, attrs[a]);
 	}
 	if (innerText !== undefined) {
-		element.innerText = innerText;
+		// Breaks on Firefox
+		//element.innerText = innerText;
+		// ???
+		//element.nodeValue = innerText;
+		// Seems the most compatible
+		element.textContent = innerText;
 	}
 	if (parent === undefined) {
 		parent = document.body;
@@ -13,55 +18,36 @@ var addElement = function(name, attrs, innerText, parent) {
 	return parent.appendChild(element);
 };
 
-// Add console.log wrapper
-var logWrapper = document.head.appendChild(
-	document.createElement('script')
-);
-logWrapper.src = 'https://raw.github.com/cpatik/console.log-wrapper/master/consolelog.min.js'; // kicks off the request
-
 // Set a title
-document.head.appendChild(
-	document.createElement('title')
-).innerText = 'Pinhead';
+addElement('title', null, 'Pinhead', document.head);
 
 // Add Bootstrap CSS
-var bootstrapCSS = document.head.appendChild(
-	document.createElement('link')
-);
-bootstrapCSS.href = 'http://twitter.github.com/bootstrap/assets/css/bootstrap.css';
-bootstrapCSS.rel = 'stylesheet'; // kicks off the request
+addElement('link', {'href':'http://twitter.github.com/bootstrap/assets/css/bootstrap.css','rel':'stylesheet'}, null, document.head);
 
 // Add Bootstrap responsive CSS
-var bootstrapResponsiveCSS = document.head.appendChild(
-	document.createElement('link')
-);
-bootstrapResponsiveCSS.href = 'http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css';
-bootstrapResponsiveCSS.rel = 'stylesheet'; // kicks off the request
+addElement('link', {'href':'http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css','rel':'stylesheet'}, null, document.head);
+// Set a viewport
+addElement('meta', {'name':'viewport', 'content':'width=device-width, initial-scale=1.0'}, null, document.head);
 
 // Add project stylesheet
-var style = document.head.appendChild(
-	document.createElement('link')
-);
-style.href = 'css/style.css';
-style.rel = 'stylesheet'; // kicks off the request
+addElement('link', {'href':'css/style.css','rel':'stylesheet'}, null, document.head);
+
+// The container div we'll stuff all our content into
+var container = addElement('div', {'class':'container'});
+addElement('h1', {'class': 'page-header'}, 'Hello World!', container);
+addElement('p', {'class': 'label label-important jqstatus'}, 'jQuery has not loaded.', container);
 
 // Add main library
-var library = document.head.appendChild(
-	document.createElement('script')
-);
+var library = addElement('script', {'src':'https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'});
+
 // Do stuff once the library has loaded
-library.addEventListener('load', function () {
-	// The container div we'll stuff all our content into
-	var container = addElement('div', {'class': 'container'});
-	addElement('h1', null, 'Hello World!', container);
-	addElement('p', {'class': 'label label-important'}, 'jQuery has not loaded.', container);
-	// Use a jQuery method to check it loaded ok
-	$('.container p.label-important').removeClass('label-important').addClass('label-success').text('jQuery loaded successfully.');
+library.addEventListener('load', function() {
+	// Use some jQuery to check it loaded ok
+	$('.jqstatus').removeClass('label-important').addClass('label-success').text('jQuery loaded successfully.');
 });
-library.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'; // kicks off the request
+
+// Add console.log wrapper
+addElement('script', {'src':'https://raw.github.com/cpatik/console.log-wrapper/master/consolelog.min.js'}, null, document.head);
 
 // Add Bootstrap plugins
-var bootstrapJS = document.head.appendChild(
-	document.createElement('script')
-);
-bootstrapJS.src = 'http://twitter.github.com/bootstrap/assets/js/bootstrap.js'; // kicks off the request
+addElement('script', {'src':'http://twitter.github.com/bootstrap/assets/js/bootstrap.js'}, null, container);
