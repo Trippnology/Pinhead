b643ad496ba8c345b389b0d4d544b16baefa0dd7
Trippnology <trippnology@gmail.com>
2014-10-21 03:26:28 +0100

    More housekeeping.

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 // Programatically add elements
 function addElement(name, attrs, innerText, parent) {
 	var element = document.createElement(name);
-	for (a in attrs) {
+	for (var a in attrs) {
 		element.setAttribute(a, attrs[a]);
 	}
 	if (innerText !== undefined) {
@@ -11,33 +11,23 @@ function addElement(name, attrs, innerText, parent) {
 		parent = document.body;
 	}
 	return parent.appendChild(element);
-};
+}
 
-// Set a title
+// HEAD
 addElement('title', null, 'Pinhead', document.head);
-
-// Add Bootstrap CSS
-addElement('link', {'href':'http://cdn.trippnology.net/css/bootstrap.min.css','rel':'stylesheet'}, null, document.head);
-
-// Set a viewport
 addElement('meta', {'name':'viewport', 'content':'width=device-width, initial-scale=1.0'}, null, document.head);
-
-// Add project stylesheet
+addElement('link', {'href':'http://cdn.trippnology.net/css/bootstrap.min.css','rel':'stylesheet'}, null, document.head);
 addElement('link', {'href':'css/style.css','rel':'stylesheet'}, null, document.head);
 
-// The container div we'll stuff all our content into
+// CONTENT
 var container = addElement('div', {'class':'container'});
 addElement('h1', {'class': 'page-header'}, 'Hello World!', container);
 addElement('p', {'class': 'label label-important jqstatus'}, 'jQuery has not loaded.', container);
 
-// Add main library
+// JS
 var library = addElement('script', {'src':'http://cdn.trippnology.net/js/jquery.min.js'});
-
-// Do stuff once the library has loaded
 library.addEventListener('load', function() {
-	// Use some jQuery to check it loaded ok
+	// Use some jQuery to show it loaded ok
 	$('.jqstatus').removeClass('label-important').addClass('label-success').text('jQuery loaded successfully.');
-
-	// Add Bootstrap plugins
 	addElement('script', {'src':'http://cdn.trippnology.net/js/bootstrap.min.js'}, null, document.body);
 });
